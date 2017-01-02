d79f49fbe7ee24efa69ff809aa0fa989dae427dd
Hüseyin Bora <hboraabaci@gmail.com>
2017-01-02 20:28:52 +0300

    Kernel Update

--- Kernel/Route.php
+++ Kernel/Route.php
@@ -225,8 +225,8 @@ class Route {
                 Route::$app_package = "Apps\\" . call_user_func_array(Route::$app_packages[$key]["app"], array());
                 Route::$app_path = BASE_DIR . DS . str_replace("\\", DS, Route::$app_package);
                 Route::$app_alias = ucfirst($key);
-                /*include Route::$app_path . DS . "Route.php";
-                include Route::$app_path . DS . "Config.php";*/
+                include Route::$app_path . DS . "Route.php";
+                include Route::$app_path . DS . "Config.php";
                 return ($key != "/") ? Catappa::getInstance()->dispatch() : $param;
             }
         }
